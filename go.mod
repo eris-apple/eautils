b830d8cb4d55cb45be13433e0b8553689a64a6dd
yushka_sidra <memusins@gmail.com>
2024-11-05 16:32:38 +0300

    init: initialize package

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/eris-apple/eautils
+
+go 1.23.1
---
+++ url.go
@@ -0,0 +1,25 @@
+package url
+
+import (
+	"net/url"
+)
+
+// NewURLConnectionString — generate a new connection string
+func NewURLConnectionString(proto, host, path, database, user, password string) string {
+	const cDataBaseURLParameter = "database"
+
+	var v = make(url.Values)
+	if len(database) > 0 {
+		v.Set(cDataBaseURLParameter, database)
+	}
+
+	var u = url.URL{
+		Scheme:   proto,
+		Host:     host,
+		Path:     path,
+		User:     url.UserPassword(user, password),
+		RawQuery: v.Encode(),
+	}
+
+	return u.String()
+}
